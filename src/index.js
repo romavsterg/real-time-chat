fae35c31e52db7b3cb8397462c97fc2177f0dd96
romavsterg <romann.mikhaylov@gmail.com>
2023-08-25 20:47:55 +0300

    added a firebase deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPoYcNFACvSXp-sTEjqI8uXChJJZ0u-O8",
+  authDomain: "real-time-chat-be592.firebaseapp.com",
+  databaseURL: "https://real-time-chat-be592-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "real-time-chat-be592",
+  storageBucket: "real-time-chat-be592.appspot.com",
+  messagingSenderId: "912700025063",
+  appId: "1:912700025063:web:0f65c83712478c8ead5a42",
+  measurementId: "G-Y3KG2K1RJ8"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const Context = createContext(null)
+
+const auth = firebase.auth()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    firebase,
+    auth
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+export async function loader() {
+    
+}
+
 function Login() {
     return (
         <div>
